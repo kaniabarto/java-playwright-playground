25d63fb30383fdab57be4670b2333ca3b400e31e
Bartłomiej Kania <kaniabarto@gmail.com>
2025-09-13 11:16:44 +0200

    add coments to explain the code

--- src/test/java/ovh/kania/m3/l4/FirstTest.java
+++ src/test/java/ovh/kania/m3/l4/FirstTest.java
@@ -1,4 +1,4 @@
-package ovh.kania.m3;
+package ovh.kania.m3.l4;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -11,11 +11,16 @@ public class FirstTest {
 
 	@Test
 	void firstTest(){
+		//Create playwright object
 		Playwright playwright =	Playwright.create();
+		//create browser object
 		Browser browser = playwright.firefox().launch();
+		//create page object
 		Page page = browser.newPage();
-
+		
+		//call navigate method on page object
 		page.navigate("https://playwright.dev/");
+		//use assertion to compare page title
 		Assertions.assertThat(page.title()).isEqualTo("Fast and reliable end-to-end testing for modern web apps | Playwright");
 	}
 }
