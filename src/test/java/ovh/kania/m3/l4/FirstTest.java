45dbbd7421a70262c0ef79bbc59fc6ea0f3ecd55
Bartłomiej Kania <kaniabarto@gmail.com>
2025-09-13 11:34:02 +0200

    headless mode and set slow motion

--- src/test/java/ovh/kania/m3/l4/FirstTest.java
+++ src/test/java/ovh/kania/m3/l4/FirstTest.java
@@ -4,6 +4,7 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.BrowserType;
 import com.microsoft.playwright.Page;
 import com.microsoft.playwright.Playwright;
 
@@ -22,5 +23,23 @@ public class FirstTest {
 		page.navigate("https://playwright.dev/");
 		//use assertion to compare page title
 		Assertions.assertThat(page.title()).isEqualTo("Fast and reliable end-to-end testing for modern web apps | Playwright");
+	}
+
+	//FIY 1 - when you run playwright on the first time, then PW will download all browsers before first test execution.
+	//FIY 2 - playwright by default run in headless mode. To see browser during test use BrowserType.LaunchOptions().setHeadless(false)
+	//FIY 3 - .setSlowMo() - method from LaunchOptions() to slow down the browser
+	
+	@Test
+	void secondTest(){
+		Playwright playwright = Playwright.create();
+		
+		//Usecase FYI 2 and 3
+		Browser browser = playwright.firefox().launch(new BrowserType.LaunchOptions().setHeadless(false).setSlowMo(1000));
+		
+		Page page = browser.newPage();
+		page.navigate("https://playwright.dev/");
+		Assertions.assertThat(page.title()).isEqualTo("Fast and reliable end-to-end testing for modern web apps | Playwright");
+
+
 	}
 }
