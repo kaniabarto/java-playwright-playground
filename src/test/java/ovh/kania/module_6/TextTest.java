77a1f786aef00503215a9986a6c654dc9062edb2
Bartłomiej Kania <kaniabarto@gmail.com>
2025-09-19 06:16:20 +0200

    text methods

---
+++ src/test/java/ovh/kania/module_6/TextTest.java
@@ -0,0 +1,14 @@
+package ovh.kania.module_6;
+
+import org.junit.jupiter.api.Test;
+
+import ovh.kania.common.BaseTest;
+
+public class TextTest extends BaseTest{
+
+	@Test
+	void textTest(){
+
+	}
+	
+}
