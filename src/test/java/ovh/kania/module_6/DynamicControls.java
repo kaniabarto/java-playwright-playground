96bc878474c648690bbd778c4ab08ecb9a04b748
Bartłomiej Kania <kaniabarto@gmail.com>
2025-09-22 22:16:01 +0200

    prep dynamic controls

---
+++ src/test/java/ovh/kania/module_6/DynamicControls.java
@@ -0,0 +1,15 @@
+package ovh.kania.module_6;
+
+import org.junit.jupiter.api.Test;
+
+import ovh.kania.common.BaseTest;
+
+public class DynamicControls extends BaseTest{
+	
+	@Test
+	void dynamicControlsTest(){
+
+
+	}
+	
+}
