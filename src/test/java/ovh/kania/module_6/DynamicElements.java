08a00eaa42c269b4bef8876ccd256457b987db25
Bartłomiej Kania <kaniabarto@gmail.com>
2025-09-22 22:14:34 +0200

    prep dynamic elements

---
+++ src/test/java/ovh/kania/module_6/DynamicElements.java
@@ -0,0 +1,13 @@
+package ovh.kania.module_6;
+
+import org.junit.jupiter.api.Test;
+
+import ovh.kania.common.BaseTest;
+
+public class DynamicElements extends BaseTest{
+
+	@Test
+	void dynamicTest(){
+
+	}
+}
